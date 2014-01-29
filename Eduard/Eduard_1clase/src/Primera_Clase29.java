6987e7ac4946cf8a9eb233519a5eced02a2cc35c
Your Name <you@example.com>
2014-01-29 08:26:46 -0600

    Clase 2

---
+++ Eduard/Eduard_1clase/src/Primera_Clase29.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+
+public class Primera_Clase29 {
+	
+	public static void main(String[] args) {
+		Scanner mi_scanner=new Scanner(System.in);
+		int a=1,mi_variable=mi_scanner.nextInt();
+		
+		if (mi_variable==10)
+		{
+			System.out.print("ES UN 10");
+		}
+		 else if (a<=10)
+		 System.out.println("No es un Valor");
+		 
+		// TODO Auto-generated method stub
+		//System.out.println("The time goes down");
+		System.out.println("Fin del Programa");
+		
+	}
+
+}
